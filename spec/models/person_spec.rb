91d7bf8443649e0e3a67a5371cb97b1d0604882a
Dan Croak <dan@thoughtbot.com>
2014-10-02 19:28:44 -0700

    Test that a Person must have a first name

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,9 +1,21 @@
 require "spec_helper"
 
 describe Person do
-  it "requires a name to be valid" do
-    # Write your spec here.
-    # You can delete the pending line.
-    pending "Not written yet"
+  it "is invalid without a first name" do
+    person = Person.new(first_name: nil)
+
+    expect(person).to be_invalid
+  end
+
+  it "is invalid with a blank first name" do
+    person = Person.new(first_name: "")
+
+    expect(person).to be_invalid
+  end
+
+  it "is valid with a first name" do
+    person = Person.new(first_name: "Cordarelle")
+
+    expect(person).to be_valid
   end
 end
